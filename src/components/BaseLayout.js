15a63c5a8f04c2b208f8a2ae2ee5c485eb61c876
Timothy <timothyleow12@gmail.com>
2024-11-23 17:21:20 +0100

    Update homepage and add chainlit copilot

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,12 +4,11 @@ import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
 import { Box, Grid } from "@mui/material";
 import MultiPageRoutes from './MultiPageRoutes';
-import { singlePage } from '../info/Info';
 import SinglePageRoutes from './SinglePageRoutes';
 import useScrollObserver from '../hooks/useScrollObserver';
 
 export default function BaseLayout() {
-   const location = useLocation()
+   const location = useLocation();
 
    const [active, setActive] = useState(location.pathname === '/' ? 'home' : location.pathname.slice(1, location.pathname.length));
    const refHome = useScrollObserver(setActive);
@@ -43,7 +42,7 @@ export default function BaseLayout() {
       } else {
          localStorage.setItem('darkMode', 'false')
       }
-   }, [])
+   }, []);
 
    useEffect(() => {
       let detectedSinglePage = JSON.parse(localStorage.getItem('singlePage'));
@@ -52,7 +51,51 @@ export default function BaseLayout() {
       } else {
          localStorage.setItem('singlePage', 'false')
       }
-   }, [])
+   }, []);
+
+   useEffect(() => {
+      // this will run when the component mounts, and when darkMode changes
+      if (document.getElementById("chainlit-copilot")) {
+         console.log("should remove old component here")
+         document.body.removeChild(document.getElementById("chainlit-copilot"));
+      }
+      const script = document.createElement("script");
+      script.src = `${process.env.REACT_APP_CHAINLIT_URL}/copilot/index.js`;
+      console.log(script.src)
+      script.async = true;
+      document.body.appendChild(script);
+
+      // Initialize the Chainlit widget after the script is loaded
+      script.onload = () => {
+         window.mountChainlitWidget({
+            chainlitServer: process.env.REACT_APP_CHAINLIT_URL,
+            theme: darkMode ? "dark" : "light",
+            button: {
+               style: {
+                  color: darkMode ? "black" : "white",
+                  bgcolor: darkMode ? "white" : "black",
+                  bgcolorHover: darkMode ? "#696969" : "grey",
+               }
+
+            }
+
+         });
+
+         // Add custom event listener for the widget
+         window.addEventListener("chainlit-call-fn", (e) => {
+            const { name, args, callback } = e.detail;
+            if (name === "test") {
+               callback("You sent: " + args.msg);
+            }
+         });
+      };
+
+      // Clean up the script and event listener on unmount
+      return () => {
+         document.body.removeChild(script);
+         window.removeEventListener("chainlit-call-fn", () => {});
+      };
+   }, [darkMode]);
 
    return (
       <Box className={darkMode ? Style.dark : Style.light}>
@@ -65,13 +108,12 @@ export default function BaseLayout() {
                {singlePage ? <SinglePageRoutes refs={{refHome, refAbout, refPortfolio, refResume}}/> : <MultiPageRoutes />}
             </Grid>
             <Grid item>
-               <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
+               <Box id="footer123" component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
                   <p>Timothy Leow &copy; 2024</p>
                </Box>
             </Grid>
          </Grid>
       </Box>
-   )
+   );
 }
-
